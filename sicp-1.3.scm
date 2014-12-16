2f164668ab64a7bf0631ea86cc2671c897c5d616
Iryna Cherniavska <icherniavska@cars.com>
2014-12-15 18:27:26 -0800

    1.3 ex

--- sicp-1.3.scm
+++ sicp-1.3.scm
@@ -72,3 +72,34 @@
         accum
         (iter (next x) (* (term x) accum))))
   (iter a 1))
+
+; Exercise 1.32.  a. Show that sum and product (exercise 1.31) are both special cases
+; of a still more general notion called accumulate that combines a collection of
+; terms, using some general accumulation function:
+; (accumulate combiner null-value term a next b)
+; Accumulate takes as arguments the same term and range specifications as sum
+; and product, together with a combiner procedure (of two arguments) that
+; specifies how the current term is to be combined with the accumulation of
+; the preceding terms and a null-value that specifies what base value to use
+; when the terms run out. Write accumulate and show how sum and product
+; can both be defined as simple calls to accumulate.
+
+; recursive
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+         (accumulate combiner null-value term (next a) next b))))
+
+; b. If your accumulate procedure generates a recursive process, write one
+; that generates an iterative process. If it generates an iterative process,
+; write one that generates a recursive process.
+; iterative
+
+(define (accumulatei combiner null-value term a next b)
+  (define (iter x accum)
+    (if (> x b)
+        accum
+        (iter (next x) (combiner (term x) accum))))
+  (iter a null-value))
+
