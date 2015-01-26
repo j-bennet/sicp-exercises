3fcb3d4777e500617b687bf6cb0580a7eb913288
Iryna Cherniavska <icherniavska@cars.com>
2015-01-26 13:52:51 -0800

    huffman tree

---
+++ sicp-2.3-huffman.scm
@@ -0,0 +1,81 @@
+#lang scheme
+; Reading: http://mitpress.mit.edu/sicp/full-text/book/book-Z-H-16.html#%_sec_2.3
+;
+; Selected exercises:
+;
+; 2.53
+; 2.54
+; 2.56
+; 2.63
+; 2.67
+;
+; Optional (but fun!): 2.68-2.70 inclusive (sha na na na na na na...)
+; ---------------- Huffman trees ----------------
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (left-branch tree) (car tree))
+
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+; The following procedure implements the decoding algorithm.
+; It takes as arguments a list of zeros and ones, together with a
+; Huffman tree.
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit -- CHOOSE-BRANCH" bit))))
+
+; Exercise 2.67.  Define an encoding tree and a sample message:
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+; Use the decode procedure to decode the message, and give the
+; result.
+(decode sample-message sample-tree)
+
+; Answer: (A D A B B C A)
