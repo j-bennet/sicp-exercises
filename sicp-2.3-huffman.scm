8e57aa2aa2215701fe7e0227f8975742fa29abc2
Iryna Cherniavska <icherniavska@cars.com>
2015-01-26 16:10:02 -0800

    ex up to 2.69

--- sicp-2.3-huffman.scm
+++ sicp-2.3-huffman.scm
@@ -79,3 +79,106 @@
 (decode sample-message sample-tree)
 
 ; Answer: (A D A B B C A)
+
+; Exercise 2.68.  The encode procedure takes as arguments a
+; message and a tree and produces the list of bits that
+; gives the encoded message.
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+; Encode-symbol is a procedure, which you must write, that
+; returns the list of bits that encodes a given symbol according
+; to a given tree. You should design encode-symbol so that it
+; signals an error if the symbol is not in the tree at all.
+; Test your procedure by encoding the result you obtained in
+; exercise 2.67 with the sample tree and seeing whether it
+; is the same as the original sample message.
+
+(define (in-list? x xs)
+  (cond [(null? xs) #f]
+        [(equal? x (car xs)) #t]
+        [else (in-list? x (cdr xs))]))
+
+;(define (encode-symbol symbol tree)
+;  (if (in-tree? symbol tree)
+;      (if (in-tree? symbol (left-branch tree))
+;          (cons 0 (encode-symbol symbol (left-branch tree)))
+;          (cons 1 (encode-symbol symbol (right-branch tree))))
+;      (error "symbol is not in tree" symbol tree)))
+
+(define (encode-symbol symbol tree)
+  (if (in-list? symbol (symbols tree))
+      (if (leaf? tree) 
+          '()
+          (let ((l (left-branch tree))
+                (r (right-branch tree)))
+            (if (in-list? symbol (symbols l))
+                (cons 0 (encode-symbol symbol l))
+                (cons 1 (encode-symbol symbol r)))))
+      (error "symbol is not in tree" symbol tree)))
+
+; Answer:
+; (encode '(A D A B B C A) sample-tree)
+; (0 1 1 0 0 1 0 1 0 1 1 1 0)
+; (0 1 1 0 0 1 0 1 0 1 1 1 0) - same as sample message
+
+; Exercise 2.69.  The following procedure takes as its argument
+; a list of symbol-frequency pairs (where no symbol appears
+; in more than one pair) and generates a Huffman encoding
+; tree according to the Huffman algorithm.
+
+;(define (generate-huffman-tree pairs)
+;  (successive-merge (make-leaf-set pairs)))
+
+; Make-leaf-set is the procedure given above that transforms
+; the list of pairs into an ordered set of leaves. Successive-merge
+; is the procedure you must write, using make-code-tree
+; to successively merge the smallest-weight elements of
+; the set until there is only one element left, which is
+; the desired Huffman tree. (This procedure is slightly tricky,
+; but not really complicated. If you find yourself designing a
+; complex procedure, then you are almost certainly doing
+; something wrong. You can take significant advantage of
+; the fact that we are using an ordered set representation.)
+
+(define (successive-merge ls)
+  (cond [(null? ls) '()]
+        [(= 1 (length ls)) (car ls)]
+        [else (successive-merge 
+               (cons
+                (make-code-tree (car ls) (cadr ls))   ; take first two
+                (cddr ls)))]))                        ; drop first two and take the rest
+
+; Exercise 2.70.  The following eight-symbol alphabet with
+; associated relative frequencies was designed to efficiently
+; encode the lyrics of 1950s rock songs. (Note that the
+; ``symbols'' of an ``alphabet'' need not be individual letters.)
+
+; A	2	NA	16
+; BOOM	1	SHA	3
+; GET	2	YIP	9
+; JOB	2	WAH	1
+; Use generate-huffman-tree (exercise 2.69) to generate a
+; corresponding Huffman tree, and use encode (exercise 2.68)
+; to encode the following message:
+
+; Get a job
+
+; Sha na na na na na na na na
+
+; Get a job
+
+; Sha na na na na na na na na
+
+; Wah yip yip yip yip yip yip yip yip yip
+
+; Sha boom
+
+; How many bits are required for the encoding?
+; What is the smallest number of bits that would be needed
+; to encode this song if we used a fixed-length code for
+; the eight-symbol alphabet?
