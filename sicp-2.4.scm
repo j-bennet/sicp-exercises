7a5f73c9a6e548d15df79e9fe4cea313ae283603
Iryna Cherniavska <i.chernyavska@gmail.com>
2015-02-11 14:26:26 -0800

    2.4 stub

---
+++ sicp-2.4.scm
@@ -0,0 +1,9 @@
+; Reading: http://mitpress.mit.edu/sicp/full-text/book/book-Z-H-17.html#%_sec_2.4
+
+; Selected exercises:
+
+; 2.73
+; 2.75
+; 2.76 -- this one will make for a good discussion on interface design!
+
+
