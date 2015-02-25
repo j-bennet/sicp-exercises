fc01724ea26f4d888827fc7bde8fb7a0b7a8c77d
Iryna Cherniavska <i.chernyavska@gmail.com>
2015-02-24 21:13:02 -0800

    some of the 3.3 ex-s

--- sicp-3.3.scm
+++ sicp-3.3.scm
@@ -1,3 +1,5 @@
+#lang scheme
+
 ; Reading: http://mitpress.mit.edu/sicp/full-text/book/book-Z-H-22.html#%_sec_3.3
 
 ; Selected exercises:
@@ -13,3 +15,32 @@
 ; for learning the basic concepts from the chapter, so it'll be up to all of
 ; you if you'd like to spend any time on them. We can definitely still discuss
 ; them at the meeting.
+
+; Exercise 3.13.  Consider the following make-cycle procedure, which uses the last-pair procedure defined in exercise 3.12:
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define (make-cycle x)
+  (set-mcdr! (last-pair x) x)
+  x)
+
+; Draw a box-and-pointer diagram that shows the structure z created by
+
+; (define z (make-cycle (list 'a 'b 'c)))
+
+; What happens if we try to compute (last-pair z)?
+
+; Answer: it would never end
+
+; Exercise 3.18.  Write a procedure that examines a list and determines whether it contains
+; a cycle, that is, whether a program that tried to find the end of the list by taking
+; successive cdrs would go into an infinite loop. Exercise 3.13 constructed such lists.
+ (define (has-cycle? l) 
+   (define (detect pair countedList) 
+     (cond ((not (pair? pair)) #f) 
+           ((memq pair countedList) #t) 
+           (else (detect (cdr pair) (cons pair countedList))))) 
+   (detect l '())) 
